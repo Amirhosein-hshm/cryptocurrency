e219dbf3854facac61d39bf15d31c0cb48c4630f
amirhosein hashemi <amirhosein.hashemi.dev@gmail.com>
2025-08-10 17:56:41 -0600

    🎉 chore(init): bootstrap Next 15 app with pnpm

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen text-green-500 flex-col p-6">hi</main>
+  );
+}
